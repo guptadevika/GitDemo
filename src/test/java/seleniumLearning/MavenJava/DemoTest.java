551f6fa6ac93b6c2e2daef287e6c617d42916e52
devika <devikagupta914@gmail.com>
2021-09-08 13:29:50 +0530

    first commit

---
+++ src/test/java/seleniumLearning/MavenJava/DemoTest.java
@@ -0,0 +1,18 @@
+package seleniumLearning.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("hello");
+	}
+	@Test
+	public void login()
+	{
+		System.out.println("login");
+	}
+
+}
